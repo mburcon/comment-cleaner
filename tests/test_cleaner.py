f3a0f21ea0b41a0b796d1171d010b946d3b1ba07
Mateusz Burcon <mburcon@sii.pl>
2022-12-28 19:51:21 +0100

    After pre-commit

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -2,13 +2,13 @@ from comment_cleaner.cleaner import clean
 
 
 def test_cleaner_removes_lines_starting_with_hash():
-    INPUT = "# This is a comment"
-    assert clean(INPUT) == ""
+    INPUT = '# This is a comment'
+    assert clean(INPUT) == ''
 
 
 def test_cleaner_removes_comments_with_leading_whitespace():
-    INPUT = "       # This is a comment"
-    assert clean(INPUT) == ""
+    INPUT = '       # This is a comment'
+    assert clean(INPUT) == ''
 
 
 def test_cleaner_does_not_remove_normal_lines():
