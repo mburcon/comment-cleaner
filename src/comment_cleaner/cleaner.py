d0bc6cff1b49b8b321554d28c8a041bc38fd8352
Mateusz Burcon <mburcon@sii.pl>
2022-12-28 20:15:27 +0100

    fixes

--- src/comment_cleaner/cleaner.py
+++ src/comment_cleaner/cleaner.py
@@ -5,27 +5,27 @@ import sys
 def clean(input: str) -> str:
     cleaned = []
     for line in input.splitlines():
-        if not line.lstrip().startswith('#'):
+        if not line.lstrip().startswith("#"):
             cleaned.append(line)
-    return '\n'.join(cleaned)
+    return "\n".join(cleaned)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', help='File to clean')
+    parser.add_argument("file", help="File to clean")
     args = parser.parse_args()
     try:
-        with open(args.file, 'r') as f:
+        with open(args.file, "r") as f:
             content = f.read()
     except FileNotFoundError:
-        print(f'File {args.file} not found')
+        print(f"File {args.file} not found")
         sys.exit(1)
 
     cleaned = clean(content)
 
-    with open(args.file, 'w+') as f:
+    with open(args.file, "w+") as f:
         f.writelines(cleaned)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
